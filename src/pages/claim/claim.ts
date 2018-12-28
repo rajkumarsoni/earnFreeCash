13d00cd632f384760a45aba31262f7e99a204937
Rajkumar Soni <rajasoni1995@gmail.com>
2018-12-29 01:08:12 +0530

    Claim page refactored

--- src/pages/claim/claim.ts
+++ src/pages/claim/claim.ts
@@ -2,21 +2,22 @@ import { Component } from '@angular/core';
 import { IonicPage, LoadingController, AlertController, NavController } from 'ionic-angular';
 import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
 import { Subscription } from 'rxjs/Rx';
-import { TotalBalance } from '../../modal/totalBalace.modal';
 import { Observable } from 'rxjs/Rx';
 import firebase from 'firebase';
 import { AdMobFreeInterstitialConfig, AdMobFree, AdMobFreeBannerConfig } from '@ionic-native/admob-free';
+import { TotalBalance } from '../../interface/total-Balance.modal';
+import { CurrentTime, ServerTime } from '../../interface/timings.interface';
 
 
-interface timeObj {
-	demoTime: any;
-}
-interface cTimeObj {
-	cTime: any;
-}
-interface yBalanceObj {
-	totalBalane: any
-}
+// interface timeObj {
+// 	demoTime: any;
+// }
+// interface cTimeObj {
+// 	cTime: any;
+// }
+// interface yBalanceObj {
+// 	totalBalane: any
+// }
 
 @IonicPage({
 	name: 'claim-game'
@@ -27,18 +28,34 @@ interface yBalanceObj {
 })
 export class ClaimPage {
 
+	/** This variable is used for storing total balance of the user */
 	totalBalance = {} as TotalBalance;
-	randomNumber: any = 1;
-	localBalance: any = 0.000001;
-	yourBalance: any = 0.0002;
-	userProfile: any = null;
-	balance = {} as yBalanceObj;
-	sub: Subscription;
-	diffTime = {} as cTimeObj;
-	currentTime: any;
-	tObj = {} as timeObj;
-	date: Date;
 
+	/** This vaiable is used for storing random numbers. */
+	randomNumber: number = 1;
+
+	/** This variable is used to store balance stored in local storage */
+	localStorageBalance: number = 0.000001;
+
+	/** This variable is used to store current balcnce */
+	usersCurrentBalance: number = 0.0002;
+
+	/** This variable is used to store user profile. */
+	userProfile: any = null;
+
+	/** This variable is used to sotre balance comming from service. */
+	balance = {} as TotalBalance;
+
+	/** This variable is used to sotre subscriptions. */
+	subscription: Subscription;
+
+	/** This variable is used to store server time coming from service. */
+	serverTime = {} as ServerTime;
+
+	/** This variable is used to sotre current device time when click on claim button. */
+	currentTime = {} as CurrentTime;
+
+	/** @ignore */
 	constructor(
 		public loadingCtrl: LoadingController,
 		private afAuth: AngularFireAuth,
@@ -47,51 +64,86 @@ export class ClaimPage {
 		public navCtrl: NavController
 	) { }
 
-	claimRipple(demoTime) {
+	/** This method is used to claim money. */
+	claimMoney() {
 		this.showInterAds();
-		if (window.localStorage.getItem("yourBalance") !== 'undefined' && window.localStorage.getItem("yourBalance") !== 'null') {
-			this.localBalance = parseFloat(window.localStorage.getItem("yourBalance"));
-		}
+		this.retriveBalanceFromLocalStorage();
 		let loader = this.loadingCtrl.create({
 			content: "Please wait...",
 		});
 		loader.present();
 		setTimeout(() => {
-			this.randomNumber = (Math.random() * (0.004 - 0.001) + 0.001).toFixed(4);
-			this.presentAlert();
-			if (this.localBalance > 0.0001) {
-				this.localBalance += parseFloat(this.randomNumber);
-				this.totalBalance.totalBalance = this.localBalance;
-				window.localStorage.setItem("yourBalance", this.totalBalance.totalBalance);
-			} else {
-				this.yourBalance += parseFloat(this.randomNumber);
-				this.totalBalance.totalBalance = this.yourBalance;
-				window.localStorage.setItem("yourBalance", this.totalBalance.totalBalance);
-			}
-			this.afAuth.authState.subscribe(auth => {
-				const yBalance: firebase.database.Reference = firebase.database().ref(`/balance/${auth.uid}`);
-				yBalance.set({
-					totalBalance: this.totalBalance.totalBalance
-				})
-			})
+			this.storingBalanceToServer();
 			loader.dismiss();
 		}, 1000);
-		// For storing current Date in server
-		this.date = new Date();
+		this.storingButtonClickedTime();
+	}
+
+	/** This method is used to store balance to server. */
+	storingBalanceToServer() {
+		this.randomNumber = parseFloat((Math.random() * (0.004 - 0.001) + 0.001).toFixed(4));
+		if (this.localStorageBalance > 0.0001) {
+			this.localStorageBalance += this.randomNumber;
+			this.totalBalance.totalBalance = this.localStorageBalance;
+			window.localStorage.setItem("usersCurrentBalance", this.totalBalance.totalBalance);
+		} else {
+			this.usersCurrentBalance += this.randomNumber;
+			this.totalBalance.totalBalance = this.usersCurrentBalance;
+			window.localStorage.setItem("usersCurrentBalance", this.totalBalance.totalBalance);
+		}
 		this.afAuth.authState.subscribe(auth => {
-			const personRef: firebase.database.Reference = firebase.database().ref(`/demoTime/${auth.uid}`);
-			personRef.set({
-				demoTime: (this.date.getTime()) + (5 * 60 * 1000)
+			const userBalance: firebase.database.Reference = firebase.database().ref(`/balance/${auth.uid}`);
+			userBalance.set({
+				totalBalance: this.totalBalance.totalBalance
+			})
+		})
+	}
+
+	/** This method is used to store current button clicked time. */
+	storingButtonClickedTime() {
+		let date = new Date();
+		this.afAuth.authState.subscribe(auth => {
+			const buttonClickTime: firebase.database.Reference = firebase.database().ref(`/buttonClickedTime/${auth.uid}`);
+			buttonClickTime.set({
+				buttonClickedTime: (date.getTime()) + (5 * 60 * 1000)
 			});
 		})
 	}
 
+	/** This method is used to retrive balance from localstorage */
+	retriveBalanceFromLocalStorage() {
+		if (window.localStorage.getItem("usersCurrentBalance") !== 'undefined' && window.localStorage.getItem("usersCurrentBalance") !== 'null') {
+			this.localStorageBalance = parseFloat(window.localStorage.getItem("usersCurrentBalance"));
+		}
+	}
+
+	/** Angular lifecycle hook. */
 	ngOnInit() {
 		this.showInterAds();
 		let loader = this.loadingCtrl.create({
 			content: "Loading...",
 		});
 		loader.present();
+		this.retriveDataFromFirebase();
+		setTimeout(() => {
+			loader.dismiss();
+		}, 2000);
+	}
+
+	/** This method is used for generating timestamp. */
+	setTimestamp() {
+		this.afAuth.authState.subscribe(data => {
+			if (data && data.email && data.uid) {
+				const personRef: firebase.database.Reference = firebase.database().ref(`/currentTime/${data.uid}`);
+				personRef.set({
+					currentTime: firebase.database.ServerValue.TIMESTAMP
+				})
+			}
+		})
+	}
+
+	/** This method is used to retive data from firebase. */
+	retriveDataFromFirebase() {
 		firebase.auth().onAuthStateChanged(user => {
 			if (user) {
 				this.userProfile = user;
@@ -109,58 +161,31 @@ export class ClaimPage {
 			}
 		});
 		let timer = Observable.timer(1, 1000);
-		this.sub = timer.subscribe(t => {
+		this.subscription = timer.subscribe(t => {
 			this.setTimestamp();
 		});
-		setTimeout(() => {
-			loader.dismiss();
-		}, 2000);
-	}
-	setTimestamp() {
-		this.afAuth.authState.subscribe(data => {
-			if (data && data.email && data.uid) {
-				const personRef: firebase.database.Reference = firebase.database().ref(`/currentTime/${data.uid}`);
-				personRef.set({
-					cTime: firebase.database.ServerValue.TIMESTAMP
-				})
-			}
-		})
 	}
 
+	/** Ionic lifecycle hook. */
 	async ionViewWillEnter() {
 		this.showBannerAds();
 		this.afAuth.authState.subscribe(data => {
 			if (data && data.email && data.uid) {
 				const abc: firebase.database.Reference = firebase.database().ref(`/currentTime/${data.uid}`);
 				abc.on('value', snapshot => {
-					this.diffTime = snapshot.val();
+					this.serverTime = snapshot.val();
 				});
-				const personRef: firebase.database.Reference = firebase.database().ref(`/demoTime/${data.uid}`);
+				const personRef: firebase.database.Reference = firebase.database().ref(`/buttonClickedTime/${data.uid}`);
 				personRef.on('value', personSnapshot => {
-					this.tObj = personSnapshot.val();
+					this.currentTime = personSnapshot.val();
 				});
 			};
 		});
 	}
 
-	presentAlert() {
-		let alert = this.alrtCtrl.create({
-			title: 'You have claimed',
-			subTitle: this.randomNumber,
-			buttons: ['ok'],
-		});
-		alert.present();
-	}
 
-	goToWithdraw(){
-		this.navCtrl.push('withdraw');
-	} 
-	goToTransaction(){
-		this.navCtrl.push('transaction-history');
-	}
 
-	// AdMob Ads Methods
-
+	/** This method is used to show banner ads. */
 	async showBannerAds() {
 		const bannerConfig: AdMobFreeBannerConfig = {
 			id: 'ca-app-pub-7368349917424686/8136698798',
@@ -176,20 +201,20 @@ export class ClaimPage {
 		}
 	}
 
-
+	/** This method is used to show interstitial ads. */
 	async showInterAds() {
-	  const interAdsConfig: AdMobFreeInterstitialConfig = {
-	    id: 'ca-app-pub-7368349917424686/1770069137',
-	    isTesting: false,
-	    autoShow: true
-	  }
-	  this.admob.interstitial.config(interAdsConfig);
-	  try {
-	    const result = this.admob.interstitial.prepare();
-	    console.log("ad" + result);
-	  } catch (e) {
-	    console.error(e);
-	  }
+		const interAdsConfig: AdMobFreeInterstitialConfig = {
+			id: 'ca-app-pub-7368349917424686/1770069137',
+			isTesting: false,
+			autoShow: true
+		}
+		this.admob.interstitial.config(interAdsConfig);
+		try {
+			const result = this.admob.interstitial.prepare();
+			console.log("ad" + result);
+		} catch (e) {
+			console.error(e);
+		}
 	}
 
 }
